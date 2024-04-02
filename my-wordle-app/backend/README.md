a72c52aa6295ae92c9be4d43f8a04074e5f02b28
Rasmus Bäckström <rasmusbaeckstrom@gmail.com>
2024-04-02 11:48:25 +0200

    Added routes with get and post

---
+++ my-wordle-app/backend/README.md
@@ -0,0 +1,8 @@
+#Wordle backend
+
+API
+
+/api/words (get)
+
+/api/highscore (get and post)
+
--- my-wordle-app/backend/index.js
+++ my-wordle-app/backend/index.js
@@ -2,8 +2,27 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+const words = [];
+const highscore = [];
+
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+});
+
+app.get('/api/words', (req, res) => {
+  res.json({ words });
+});
+
+app.get('/api/highscore', (req, res) => {
+  res.json({ highscore });
+});
+
+app.post('/api/highscore', (req, res) => {
+  const newScore = req.body;
+  highscore.push(newScore);
+  res.status(201).send("Highscore added :)");
 });
 
 app.listen(5080);
